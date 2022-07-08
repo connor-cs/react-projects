91b86d258a57427a367124884b8ab7d46b84ebea
Connor <connor.cs@protonmail.com>
2022-07-08 14:22:49 -0400

    done

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
+import questions from './data';
 import data from './data';
+import Question from './Question';
 import SingleQuestion from './Question';
+
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data)
+  return (
+    <div className='container'>
+      <h3>questions and answers</h3>
+      <section className='info'>
+        {questions.map(ques => {
+          return <Question key={ques.id} {...ques}/>
+        })}
+      </section>
+    </div>
+    );
 }
 
 export default App;
+
+
+//iterate over list of questions
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+
+const Question = ({ id, title, info }) => {
+
+  const [active, setActive] = useState(false)
+
+  function handleClick() {
+    setActive(active=>!active)
+  }
+  
+  return (
+    <article className='question' key={id}>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={handleClick}>
+          {active ? <AiOutlineMinus/> : <AiOutlinePlus/>}</button>
+        
+      </header>
+      {active ? <p>{info}</p> : null}
+    </article>
+  )
 };
 
 export default Question;
